296baa3ae6157b58c8752cd57b6acdd3237fdad2
hunx <junaidhunain6@gmail.com>
2024-05-21 20:46:46 +0500

    added the auto configure handler and made it dynamically functional

--- thedocks.jsx
+++ thedocks.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-export default function Thedock({configuration, style, iconHeight, iconWidth, maxBoxes, autoArrange, containerHeight, setpercentage, percentage, containerWidth, iconstyles, icons, startingPosition}){
+export default function Thedock({ configuration, style, iconHeight, iconWidth, maxBoxes, autoArrange, containerHeight, setpercentage, percentage, containerWidth, iconstyles, icons, startingPosition }) {
 
     //STATES
     const [heigth, setheight] = useState(0)
@@ -7,13 +7,14 @@ export default function Thedock({configuration, style, iconHeight, iconWidth, ma
     const [allIconsDestiny, setAllIconsDestiny] = useState([]);
 
 
+
     const max = (arr) => {
-        let temp = arr.map(elem=>elem);
-        temp.sort((a, b)=>(a - b));
+        let temp = arr.map(elem => elem);
+        temp.sort((a, b) => (a - b));
         return temp[temp.length - 1];
     }
 
-      //STYLES
+    //STYLES
     const stylings = {
         main: {
             width: containerWidth,
@@ -34,21 +35,21 @@ export default function Thedock({configuration, style, iconHeight, iconWidth, ma
     }
 
     //the bounce
-    React.useEffect(()=>{
+    React.useEffect(() => {
 
-        if(percentage == 1.1){
+        if (percentage == 1.1) {
             setTimeout(() => {
                 setpercentage(0.9)
             }, 100);
-        }else if(percentage == 0.9){
+        } else if (percentage == 0.9) {
             setTimeout(() => {
                 setpercentage(1.05)
             }, 100);
-        }else if(percentage == 1.05){
+        } else if (percentage == 1.05) {
             setTimeout(() => {
                 setpercentage(0.95)
             }, 100);
-        }else if(percentage == 0.95){
+        } else if (percentage == 0.95) {
             setTimeout(() => {
                 setpercentage(1)
             }, 100);
@@ -63,143 +64,166 @@ export default function Thedock({configuration, style, iconHeight, iconWidth, ma
         //     }, 30);
         // }
     }, [percentage])
-    
 
     //make the virtual boxes
-    React.useEffect(()=>{
-    //determine the max numebr of boxes in a row
-    let maxBoxes = max(configuration);
-    //now divide this by the container's width    
-    let standard_width = document.querySelector('.thedock_container').clientWidth;
-    let boxwidth = standard_width / maxBoxes 
-    //height of box = width of box
-    let standard_height = document.querySelector('.thedock_container').clientHeight
-    let boxHeight = standard_height / configuration.length
-    console.log(boxHeight, standard_height)
-    //so each box in any row will have this clientWidth, now get the coords of the container
-    let containerXstart = document.querySelector('.thedock_container').offsetLeft;
-    let containerXend = containerXstart + standard_width;
-    let containerYstart =  document.querySelector('.thedock_container').offsetTop;
-    let containerYend  = containerYstart + standard_height
-    // now begin the math
-    // row by row
-    let allcords = []
-    configuration.forEach((row, rowindex) => {
-        //count the excess space
-        // let rowCords = [];
-        let excessSpace = standard_width - row * boxwidth
-        //now get the coordiantes of start of first icon and end of last icon
-        let firsticonstart = containerXstart + excessSpace/2;
-        let lasticonend = containerXend - excessSpace/2;
-        //now get the destiny spots: boxwidth/2 + a x boxwidth
-        for(let a = 0; a < row; a++){
-            let currentCoordinates = []
-            //x coordinate
-            currentCoordinates.push(firsticonstart + boxwidth/2 + a*boxwidth)
-            //y coordinate
-            currentCoordinates.push(containerYstart + boxHeight/2 + rowindex*boxHeight)
-            allcords.push(currentCoordinates)
-            console.log(containerYstart)
-            console.log(currentCoordinates)
+    React.useEffect(() => {
+        let tempconfiguration = configuration !== undefined ? configuration : [maxBoxes];
 
+
+        //auto configuration handler
+        if (!configuration && maxBoxes) {
+            configHandler(maxBoxes - 1, icons.length - maxBoxes)
         }
-        // allcords.push(rowCords)
 
-    })  
-    
-    //now begin preparing the icons and thier positions
-    let coords = [];
-    let alignment = []
-    let directionx = 0;
-    let directiony = 0;
-    if(startingPosition === 'top-left'){
-        coords = [containerXstart + 0, containerYstart + 0];
-        alignment = ['start', 'start'];
-        directionx = -1;
-        directiony = -1
-    }else if(startingPosition === 'top-center'){
-        coords = [containerXstart + standard_width/2, containerYstart + 0];
-        alignment = ['start', 'center'];
-        directiony = - 1
-    }else if(startingPosition === 'top-right'){
-        coords = [containerXstart + standard_width, containerYstart + 0];
-        alignment = ['start', 'end'];
-        directiony = -1;
-        directionx = 1
-    }else if(startingPosition === 'middle-left'){
-        coords = [containerXstart + 0, containerYstart + standard_height/2];
-        alignment = ['center', 'start'];
-        directionx = -1
-    }else if(startingPosition === 'middle-center'){
-        coords = [containerXstart + standard_width/2, containerYstart + standard_height/2];
-        alignment = ['center', 'center'];
-    }else if(startingPosition === 'middle-right'){
-        coords = [containerXstart + standard_width, containerYstart + standard_height/2];
-        alignment = ['center', 'end'];
-        directionx = 1
-    }else if(startingPosition === 'bottom-left'){
-        coords = [containerXstart + 0, containerYstart + standard_height];
-        alignment = ['end', 'start'];
-        directionx = -1;
-        directiony = 1
-    }else if(startingPosition === 'bottom-center'){
-        coords = [containerXstart + standard_width/2, containerYstart + standard_height];
-        alignment = ['end', 'center'];
-        directiony = 1
-    }else if(startingPosition === 'bottom-right'){
-        coords = [containerXstart + standard_width, containerYstart + standard_height];
-        alignment = ['end', 'end'];
-        directionx = 1;
-        directiony = 1
-    }
+        //this took around an hour
+        let onePosition = false
+        function configHandler(n, max) {
+            if (max <= 0 || n < 0) {
+                return
 
-    //initialCords of all the icons are available, apply vectors anf get final coords
+            }
+            if (max - n * 2 >= 0 && n !== 0) {
+                tempconfiguration.unshift(n);
+                tempconfiguration.push(n);
+                onePosition = n == 1
+                configHandler(n - 1, max - n * 2)
+            } else {
+                n > 1 ? configHandler(n - 1, max) : (onePosition ? tempconfiguration[0]++ : tempconfiguration.unshift(n))
 
-
-    let allIconsOnDestiny = icons.map((iconSRC, index) => {
-        //  PD = OD - OP, Im sorry but maths is hard to read ngl
-        let OD = allcords[index];
-        let OP = coords
-        let PD = [OD[0] - OP[0], OD[1] - OP[1]];
-        //now subtract the icon's own how
-        
-
-        PD[0] = Math.round(PD[0] + directionx*iconWidth/2);
-        PD[1] = Math.round(PD[1] + directiony*iconHeight/2);
-        console.log(PD)
-
-        
-        
-    
-        //voila, now we know how much each point has to travel from P!
-        return {
-            Cmpnt: ({transStyles}) => (<img className="thedock_icon" style={{...stylings.icon, ...transStyles, ...iconstyles}} src={iconSRC} />),
-            destiny: PD
+            }
         }
-    })
+
+        //determine the max numebr of boxes in a row
+        let PotentiallyMaxBoxes = max(tempconfiguration);
+        //now divide this by the container's width    
+        let standard_width = document.querySelector('.thedock_container').clientWidth;
+        let boxwidth = standard_width / PotentiallyMaxBoxes
+        //height of box = width of box
+        let standard_height = document.querySelector('.thedock_container').clientHeight
+        let boxHeight = standard_height / tempconfiguration.length
+        //so each box in any row will have this clientWidth, now get the coords of the container
+        let containerXstart = document.querySelector('.thedock_container').offsetLeft;
+        let containerXend = containerXstart + standard_width;
+        let containerYstart = document.querySelector('.thedock_container').offsetTop;
+        let containerYend = containerYstart + standard_height
+        // now begin the math
+        // row by row
+        let allcords = []
+        tempconfiguration.forEach((row, rowindex) => {
+            //count the excess space
+            // let rowCords = [];
+            let excessSpace = standard_width - row * boxwidth
+            //now get the coordiantes of start of first icon and end of last icon
+            let firsticonstart = containerXstart + excessSpace / 2;
+            let lasticonend = containerXend - excessSpace / 2;
+            //now get the destiny spots: boxwidth/2 + a x boxwidth
+            for (let a = 0; a < row; a++) {
+                let currentCoordinates = []
+                //x coordinate
+                currentCoordinates.push(firsticonstart + boxwidth / 2 + a * boxwidth)
+                //y coordinate
+                currentCoordinates.push(containerYstart + boxHeight / 2 + rowindex * boxHeight)
+                allcords.push(currentCoordinates)
+
+            }
+            // allcords.push(rowCords)
+
+        })
+
+        //now begin preparing the icons and thier positions
+        let coords = [];
+        let alignment = []
+        let directionx = 0;
+        let directiony = 0;
+        if (startingPosition === 'top-left') {
+            coords = [containerXstart + 0, containerYstart + 0];
+            alignment = ['start', 'start'];
+            directionx = -1;
+            directiony = -1
+        } else if (startingPosition === 'top-center') {
+            coords = [containerXstart + standard_width / 2, containerYstart + 0];
+            alignment = ['start', 'center'];
+            directiony = - 1
+        } else if (startingPosition === 'top-right') {
+            coords = [containerXstart + standard_width, containerYstart + 0];
+            alignment = ['start', 'end'];
+            directiony = -1;
+            directionx = 1
+        } else if (startingPosition === 'middle-left') {
+            coords = [containerXstart + 0, containerYstart + standard_height / 2];
+            alignment = ['center', 'start'];
+            directionx = -1
+        } else if (startingPosition === 'middle-center') {
+            coords = [containerXstart + standard_width / 2, containerYstart + standard_height / 2];
+            alignment = ['center', 'center'];
+        } else if (startingPosition === 'middle-right') {
+            coords = [containerXstart + standard_width, containerYstart + standard_height / 2];
+            alignment = ['center', 'end'];
+            directionx = 1
+        } else if (startingPosition === 'bottom-left') {
+            coords = [containerXstart + 0, containerYstart + standard_height];
+            alignment = ['end', 'start'];
+            directionx = -1;
+            directiony = 1
+        } else if (startingPosition === 'bottom-center') {
+            coords = [containerXstart + standard_width / 2, containerYstart + standard_height];
+            alignment = ['end', 'center'];
+            directiony = 1
+        } else if (startingPosition === 'bottom-right') {
+            coords = [containerXstart + standard_width, containerYstart + standard_height];
+            alignment = ['end', 'end'];
+            directionx = 1;
+            directiony = 1
+        }
+
+        //initialCords of all the icons are available, apply vectors anf get final coords
 
 
-    setAlignment(alignment)
-    setAllIconsDestiny(allIconsOnDestiny)
-    setheight(standard_height)
-    
-    }, [configuration, containerWidth, icons, startingPosition, containerHeight]);
+        let allIconsOnDestiny = icons.map((iconSRC, index) => {
+            //  PD = OD - OP, Im sorry but maths is hard to read ngl
+            let OD = allcords[index];
+            let OP = coords
+            let PD = [OD[0] - OP[0], OD[1] - OP[1]];
+            //now subtract the icon's own how
+
+
+            PD[0] = Math.round(PD[0] + directionx * iconWidth / 2);
+            PD[1] = Math.round(PD[1] + directiony * iconHeight / 2);
+
+
+
+
+            //voila, now we know how much each point has to travel from P!
+            return {
+                Cmpnt: ({ transStyles }) => (<img className="thedock_icon" style={{ ...stylings.icon, ...transStyles, ...iconstyles }} src={iconSRC} />),
+                destiny: PD
+            }
+        })
+
+
+        setAlignment(alignment)
+        setAllIconsDestiny(allIconsOnDestiny)
+        setheight(standard_height)
+
+
+    }, [configuration, maxBoxes, containerWidth, icons, startingPosition, containerHeight]);
 
 
 
     return (
-       <div style={{...stylings.main, ...style}} className="thedock_container">
-        {
-            allIconsDestiny.map(({Cmpnt, destiny}) => {
-                return (<Cmpnt 
-                    transStyles={
-                        {transform: 
-                            `translate(${  destiny[0] * percentage   }px, ${   destiny[1] * percentage  }px)`
+        <div refr={allIconsDestiny} style={{ ...stylings.main, ...style }} className="thedock_container">
+            {
+                allIconsDestiny.map(({ Cmpnt, destiny }) => {
+                    return (<Cmpnt
+                        transStyles={
+                            {
+                                transform:
+                                    `translate(${destiny[0] * percentage}px, ${destiny[1] * percentage}px)`
+                            }
                         }
-                    } 
                     />)
-            })
-        }
-       </div>
+                })
+            }
+        </div>
     )
 }
